89dfd041f2a4922af0ce59f845b3ad98d346756e
krisgale <63319327+krisgale@users.noreply.github.com>
2023-08-14 17:55:49 +0000

    lesson 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Welcome from './components/Welcome';
 import Click from './components/Click';
+import Cast from './components/Cast';
 
 function App() {
   const name = 'StarGazers'
@@ -12,6 +13,7 @@ function App() {
           <Welcome name={ name } style={ nameStyle } />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <Cast />
           <Click />
         </hgroup>
       </article>
